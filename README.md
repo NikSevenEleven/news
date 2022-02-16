54bfb875a125ac79257613dbb92d9e87ee302f9d
NikSevenEleven <NikSevenEleven>
2022-02-16 14:14:11 +0300

    first commit

--- README.md
+++ README.md
@@ -67,3 +67,4 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
 # Blog
 # Blog
 # Blog
+# Для реализации имеющего функционала нужно пройти на админку и зарегистрироваться, пользователь по умолчанию админ
--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -9,6 +9,9 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-         return redirect()->route('post.index');
+        $posts=Post::paginate(6);
+        $randomPosts = Post::get()->random(4);
+        $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count','DESC')->get()->take(4);
+        return view('main.index',compact('posts','randomPosts','likedPosts'));
     }
 }
